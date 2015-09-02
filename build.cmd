0209f6897f05aae1ffc3a4b7a0fed4709c8cb1d5
Wes Day <wespday@users.noreply.github.com>
2015-09-01 19:29:29 -0500

    Add initial source code.

---
+++ build.cmd
@@ -0,0 +1,31 @@
+@SETLOCAL
+
+@SET SRC=%~dp0\src
+@SET ARTIFACTS=%~dp0\artifacts
+@SET NUGET_COMMAND=%SRC%\.nuget\nuget.exe
+@SET PACKAGE_VERSION=2.0.0
+@SET NUGET_PACKAGE_ID=CategoryTraits.xUnit2
+@SET SOLUTION=%SRC%\CategoryTraits.xUnit2.sln
+@SET PROJECT_FOLDER=%SRC%\%NUGET_PACKAGE_ID%
+@SET MSBUILDARGS=/target:Rebuild /fileLogger /verbosity:minimal /ToolsVersion:14.0
+@SET NUGET_PACKAGE_FOLDER=%ARTIFACTS%\%NUGET_PACKAGE_ID%
+@SET DOTNET_FRAMEWORK=portable-net45+win+wpa81+wp80
+@SET NUGET_PORTABLE_FRAMEWORK_FOLDER=%NUGET_PACKAGE_FOLDER%\lib\%DOTNET_FRAMEWORK%
+
+RMDIR /Q /S "%ARTIFACTS%"
+%NUGET_COMMAND% restore "%SOLUTION%"  -Verbosity quiet ||  GOTO BuildFailed
+MSBuild "%SOLUTION%" %MSBUILDARGS% ||  GOTO BuildFailed
+MSBuild "%SOLUTION%" %MSBUILDARGS% /property:Configuration=Release ||  GOTO BuildFailed
+MKDIR "%NUGET_PORTABLE_FRAMEWORK_FOLDER%" ||  GOTO BuildFailed
+COPY "%PROJECT_FOLDER%\bin\Release\%NUGET_PACKAGE_ID%.??l" "%NUGET_PACKAGE_FOLDER%\lib\%DOTNET_FRAMEWORK%\" ||  GOTO BuildFailed
+COPY "%PROJECT_FOLDER%\bin\Release\%NUGET_PACKAGE_ID%.nuspec" "%NUGET_PACKAGE_FOLDER%\" ||  GOTO BuildFailed
+%NUGET_COMMAND% pack "%NUGET_PACKAGE_FOLDER%\%NUGET_PACKAGE_ID%.nuspec" -Version %PACKAGE_VERSION% -NonInteractive -OutputDirectory %ARTIFACTS% -Properties version=%PACKAGE_VERSION%||  GOTO BuildFailed
+
+@ECHO.
+@ECHO **** BUILD SUCCESSFUL ****
+GOTO:EOF
+
+:BuildFailed
+@ECHO.
+@ECHO *** BUILD FAILED ***
+EXIT /B -1
---
+++ src/CategoryTraits.Xunit2/CategoryTraitDiscoverer.cs
@@ -0,0 +1,70 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CategoryTraitDiscoverer.cs" company="Wes Day">
+//   The MIT License (MIT)
+//   
+//   Copyright (c) 2015 Wes Day
+//   
+//   Permission is hereby granted, free of charge, to any person obtaining a copy of
+//   this software and associated documentation files (the "Software"), to deal in
+//   the Software without restriction, including without limitation the rights to
+//   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+//   the Software, and to permit persons to whom the Software is furnished to do so,
+//   subject to the following conditions:
+//   
+//   The above copyright notice and this permission notice shall be included in all
+//   copies or substantial portions of the Software.
+//   
+//   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+//   FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+//   COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+//   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CategoryTraits.Xunit2
+{
+    using System.Collections.Generic;
+
+    using Xunit.Abstractions;
+    using Xunit.Sdk;
+
+    /// <summary>
+    /// This class discovers all of the xUnit tests and test classes that have
+    /// applied the TraitDiscoverer attribute for this trait discoverer.
+    /// This class is referenced for example by the Visual Studio test explorer to discover test traits such as unit tests or integration tests.
+    /// Originally derived from the <a href="https://github.com/xunit/samples.xunit/tree/master/TraitExtensibility">xUnit TraitExtensibility Sample</a>.
+    /// </summary>
+    public class CategoryTraitDiscoverer : ITraitDiscoverer
+    {
+        /// <summary>
+        /// The namespace of this class
+        /// </summary>
+        internal const string Namespace = nameof(CategoryTraits) + "." + nameof(Xunit2);
+
+        /// <summary>
+        /// The namespace of this class
+        /// </summary>
+        internal const string FullyQualifiedName = Namespace + "." + nameof(CategoryTraitDiscoverer);
+
+        /// <summary>
+        /// Gets the trait values from the Category attribute.
+        /// </summary>
+        /// <param name="traitAttribute">
+        /// The trait attribute containing the trait values.
+        /// </param>
+        /// <returns>
+        /// The trait values.
+        /// </returns>
+        public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+        {
+            var categoryValue = traitAttribute.GetNamedArgument<string>("Category");
+
+            if (!string.IsNullOrWhiteSpace(categoryValue))
+            {
+                yield return new KeyValuePair<string, string>("Category", categoryValue);
+            }
+        }
+    }
+}
